99f4ffc12807ba87a2e6afdd9804b7f2eddacffe
kahan-sonani <kahansonani1008@gmail.com>
2022-02-18 01:14:38 +0530

    deployed - Azure

--- TheOrchidApp/settings.py
+++ TheOrchidApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -118,8 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+DEFAULT_FILE_STORAGE = 'backend.custom_azure.AzureMediaStorage'
+STATICFILES_STORAGE = 'backend.custom_azure.AzureStaticStorage'
+
+MEDIA_LOCATION = "media"
+STATIC_LOCATION = "static"
+
+AZURE_ACCOUNT_NAME = "djangotheorchidstorage"
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
---
+++ backend/custom_azure.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+from storages.backends.azure_storage import AzureStorage
+
+
+class AzureStaticStorage(AzureStorage, ABC):
+    account_name = 'djangotheorchidstorage'
+    account_key = 'Pz42XCfcqOx3gIOa5UiE2FxpIL/O+U+K84tQzBZFhpGzByOOkYYuSZ6JohOZLGTZ3iI6wWcJOsv0M/Cm0u6v8Q=='
+    azure_container = 'static'
+    expiration_secs = None
+
+
+class AzureMediaStorage(AzureStorage, ABC):
+    account_name = 'djangotheorchidstorage'
+    account_key = 'Pz42XCfcqOx3gIOa5UiE2FxpIL/O+U+K84tQzBZFhpGzByOOkYYuSZ6JohOZLGTZ3iI6wWcJOsv0M/Cm0u6v8Q=='
+    azure_container = 'media'
+    expiration_secs = None
