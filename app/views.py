bfcef2023a161d1fa442c32746a402e120c77f79
kahan-sonani <kahansonani1008@gmail.com>
2022-02-25 18:39:18 +0530

    deployed - Azure

--- app/views.py
+++ app/views.py
@@ -29,41 +29,41 @@ def logout_user(request):
 
 def contacts(request):
 
-    connections = None
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        service = build('people', 'v1', credentials=creds)
-
-        results = service.people().connections().list(
-            resourceName='people/me',
-            pageSize=10,
-            personFields='names,emailAddresses').execute()
-        connections = results.get('connections', [])
-
-        for person in connections:
-            names = person.get('names', [])
-            if names:
-                name = names[0].get('displayName')
-                print(name)
-    except HttpError as err:
-        print(err)
-        messages.warning(request, "Something went wrong!, Couldn't load contacts");
+    # connections = None
+    # creds = None
+    # # The file token.json stores the user's access and refresh tokens, and is
+    # # created automatically when the authorization flow completes for the first
+    # # time.
+    # if os.path.exists('token.json'):
+    #     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # # If there are no (valid) credentials available, let the user log in.
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             'credentials.json', SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     # Save the credentials for the next run
+    #     with open('token.json', 'w') as token:
+    #         token.write(creds.to_json())
+    #
+    # try:
+    #     service = build('people', 'v1', credentials=creds)
+    #
+    #     results = service.people().connections().list(
+    #         resourceName='people/me',
+    #         pageSize=10,
+    #         personFields='names,emailAddresses').execute()
+    #     connections = results.get('connections', [])
+    #
+    #     for person in connections:
+    #         names = person.get('names', [])
+    #         if names:
+    #             name = names[0].get('displayName')
+    #             print(name)
+    # except HttpError as err:
+    #     print(err)
+    #     messages.warning(request, "Something went wrong!, Couldn't load contacts")
 
     return render(request, 'contacts.html', connections)
