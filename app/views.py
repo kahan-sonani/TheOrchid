b3783428069d3c0d7fea46da8dfce4b042a1f020
kahan-sonani <kahansonani1008@gmail.com>
2022-02-28 14:54:40 +0530

    deployed - Azure

--- app/views.py
+++ app/views.py
@@ -40,7 +40,7 @@ def contacts(request):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                '../client_secret.json', SCOPES)
+                '../static/client_secret.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
