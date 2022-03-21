51d0f7740ed6dcd65538134f0bc5667de39afa8c
kahan-sonani <kahansonani1008@gmail.com>
2022-03-21 20:33:53 +0530

    call-management websocket - done

--- TheOrchidApp/settings.py
+++ TheOrchidApp/settings.py
@@ -79,19 +79,19 @@ ASGI_APPLICATION = 'TheOrchidApp.routing.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
-}
 # CHANNEL_LAYERS = {
 #     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [("localhost", 6379)],
-#         },
-#     },
+#         "BACKEND": "channels.layers.InMemoryChannelLayer"
+#     }
 # }
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],
+        },
+    },
+}
 
 DATABASES = {
     'default': {
@@ -142,13 +142,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# STATICFILES_STORAGE = 'backend.custom_azure.AzureStaticStorage'
-# STATIC_LOCATION = "static"
-# AZURE_ACCOUNT_NAME = "djangotheorchidstorage"
-# AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
-# STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+STATICFILES_STORAGE = 'backend.custom_azure.AzureStaticStorage'
+STATIC_LOCATION = "static"
+AZURE_ACCOUNT_NAME = "djangotheorchidstorage"
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 
 SECURE_REFERRER_POLICY = 'no-referrer-when-downgrade'
 AUTH_USER_MODEL = 'landing.OUser'
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
